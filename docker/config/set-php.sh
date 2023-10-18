b73569dd418146ef8cc1283f03345555f50a5e82
mig-ll <vsantos.mv@gmail.com>
2023-10-18 16:18:41 -0600

    enable php 8

--- docker/config/set-php.sh
+++ docker/config/set-php.sh
@@ -2,9 +2,15 @@
 version=$1
 
 if [ "$version" = "" ]; then
-    echo "--------------------------------------------------------------"
-    echo "No PHP version specified. Available versions 5[5.6] and 7[7.0]"
-    echo "--------------------------------------------------------------"
+    echo "-----------------------------------------------------------------------"
+    echo "No PHP version specified. Available versions 5[5.6] , 8[8.2] and 7[7.0]"
+    echo "-----------------------------------------------------------------------"
+elif [ "$version" = "8" ]; then
+    rm /etc/alternatives/php
+    ln -s /usr/bin/php8.2 /etc/alternatives/php
+    echo "--------------------"
+    echo "PHP Version set to 8"
+    echo "--------------------"
 elif [ "$version" = "5" ]; then
     rm /etc/alternatives/php
     ln -s /usr/bin/php5.6 /etc/alternatives/php
